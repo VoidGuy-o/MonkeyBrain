447703cf17d839146ae33014b1b5bcefffc24d79
Void <void.guyl1@gmail.com>
2026-07-30 00:20:27 +0300

    module 4: trying to figure out how to handle BMP. Added imageio for image reading/writing. Made a little setup that makes first pixel green. WIP

--- app/lab4module/logic.py
+++ app/lab4module/logic.py
@@ -1,5 +1,25 @@
 from tkinter import ttk, filedialog as fd
+import imageio.v3 as iio
 
 
-def encode(message: str, BMPfilepath:str) -> any:
-    
+def encode(message: str, BMPfilepath: str) -> any:
+
+    imagebitmap_ndarray = iio.imread(BMPfilepath)
+    print(f"look: {imagebitmap_ndarray[0][0]}")
+    imagebitmap_ndarray[0][0] = [0, 200, 0]
+    print(f"look: {imagebitmap_ndarray[0][0]}")
+    print(message)
+    print(BMPfilepath)
+    print(imagebitmap_ndarray)
+    print(f"image type: {type(imagebitmap_ndarray)}")
+    print(f"image size/shape: {imagebitmap_ndarray.shape}")
+    iio.imwrite(r"", imagebitmap_ndarray) # insert your own path/URI
+
+
+
+
+
+if __name__ == "__main__":
+    test_message: str = "Hello world"
+    filepath: str =r"" #insert your own path/URI
+    encode(test_message, filepath)

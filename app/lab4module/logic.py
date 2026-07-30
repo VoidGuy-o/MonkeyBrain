52d311098fd154595c0f5bd7fb661d8cbd2c399b
Void <void.guyl1@gmail.com>
2026-07-30 20:51:54 +0300

    module 4 logic: algo for stegraphy - done. + added json table (Eng+Ukr alphabet + some symbols)

--- app/lab4module/logic.py
+++ app/lab4module/logic.py
@@ -1,12 +1,42 @@
 from tkinter import ttk, filedialog as fd
+import json
 import imageio.v3 as iio
 
 
-def encode(message: str, BMPfilepath: str) -> any:
 
+def encode(message: str, BMPfilepath: str) -> any:
+    message = message.lower()
     imagebitmap_ndarray = iio.imread(BMPfilepath)
+    with open(r"app\lab4module\dictionary_table.json", mode = "r", encoding = "utf-8") as dictionary_table:
+        convertion_table = json.load(dictionary_table)
+    message_array = [convertion_table[letter] for letter in message]
+    message_array = [str(letter) for letter in message_array]
+    message_array = "".join(message_array)
+    message_array_final = [int(symbol) for symbol in message_array]
+
+    letter_num = 0
+    for item_a in imagebitmap_ndarray:
+        if letter_num == len(message_array):
+                        break
+        for item_b in item_a:
+            if letter_num == len(message_array):
+                break
+            while (item_b[0] + item_b[1] + item_b[2]) % 10 != message_array_final[letter_num]:
+                item_b[0] +=1
+            letter_num += 1
+
+
+
+
+
+
+
+
+
+
+    print(f"convertion table dict: {convertion_table}")
+    print(f"message converted to list of ints: {message_array}")
     print(f"look: {imagebitmap_ndarray[0][0]}")
-    imagebitmap_ndarray[0][0] = [0, 200, 0]
     print(f"look: {imagebitmap_ndarray[0][0]}")
     print(message)
     print(BMPfilepath)

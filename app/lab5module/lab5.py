587587151770f0ff8eedfca5b0f0093166df90a9
Void <void.guyl1@gmail.com>
2026-08-04 01:00:17 +0300

    minor fixes

--- app/lab5module/lab5.py
+++ app/lab5module/lab5.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
-from lab5module.logic import encode
+#from lab5module.logic import encode
 
 
 
 
-class Frame4(ttk.Frame):
+class Frame5(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
         self.config(width=400, height=200)
--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from lab1module.lab1 import Frame1
 from lab2module.lab2 import Frame2
 from lab3module.lab3 import Frame3
 from lab4module.lab4 import Frame4
-#from lab5module.lab5 import Frame5
+from lab5module.lab5 import Frame5
 #from lab6module.lab6 import Frame6
 
 
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     Frame2 = Frame2(App)
     Frame3 = Frame3(App)
     Frame4 = Frame4(App)
-    #Frame5 = Frame5(App)
+    Frame5 = Frame5(App)
     #Frame6 = Frame6(App)
     App.mainloop()

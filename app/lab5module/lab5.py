dd81945b602d1b46f51526363c33726de5c0559f
VoidGuy-o <void.guyl1@gmail.com>
2026-09-20 03:21:46 +0300

    module 5: reworked logic, removed 'func' function, made key generation. connected everything to the GUI

--- app/lab5module/lab5.py
+++ app/lab5module/lab5.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-#from lab5module.logic import encode
-
-
+from lab5module.logic import encrypt
 
 
 class Frame5(ttk.Frame):
@@ -15,3 +13,32 @@ class Frame5(ttk.Frame):
 
         container.notebook.add(self, text="Lab 5")
 
+        self.encoded_text_field = tk.Text(self, width = 100, height = 5)
+        self.encoded_text_field.pack(side="bottom", fill="x")
+
+        self.label_result = ttk.Label(self, text="Result")
+        self.label_result.pack(side="bottom",pady=5, anchor="w")
+
+        self.start_button = ttk.Button(self, text="Encode", command=lambda: self.entry_result_command(self.input_text_field, int(self.spinbox_shift.get())))
+        self.start_button.pack(side="bottom", pady=0, anchor="center")
+
+
+        self.input_text_field = tk.Text(self, width=100, height=5)
+        self.input_text_field.pack(side="bottom", fill="x")
+
+        
+        self.label_input = ttk.Label(self, text="Message you want to encode")
+        self.label_input.pack(side="bottom", pady=5, anchor="w")
+
+        self.spinbox_shift = ttk.Spinbox(self, from_ = 0, to=100, wrap=True, width=5)
+        self.spinbox_shift.pack(side="left", pady=10,padx=10)
+
+
+    # i have no idea what is this, I just copied it from the lab2 
+    def entry_result_command(self, from_, int_):
+        extracted_message = from_.get(1.0, 'end')
+        text = encrypt(extracted_message[:-1], int_, debug=True)
+        self.encoded_text_field.delete(1.0, 'end')
+        self.encoded_text_field.insert(index=1.0, chars=text)
+        print(self.spinbox_shift.get())
+        print(type(self.spinbox_shift.get()))
--- app/lab5module/logic.py
+++ app/lab5module/logic.py
@@ -1,14 +1,7 @@
 import random
 
 
-def func(side: str, key: str):
-    #side = side.replace("1", "2")
-    #side = side.replace("0", "1")
-    #side = side.replace("2", "0")
-    list_side = list(side)
-    random.shuffle(list_side)
-    side = "".join(list_side)
-    return side
+# XOR function
 def XOR(lefts, rights):
     result = []
     for i in range(int(len(lefts))):
@@ -23,33 +16,42 @@ def XOR(lefts, rights):
     return "".join(result)
 
     
-
-
-
-
-
-def encrypt(message: str, rounds: int = 1):
+def encrypt(message: str, rounds: int = 1, debug=False):
+    # convering the str input into binary str, slicing into two halves
     binary_message = ''.join(format(ord(char), '08b') for char in message)
     half = int(len(binary_message)/2)
     left = binary_message[:half]
     right = binary_message[half:]
-    k = 1
-    print(f"left: {left} \nright:{right}\n")
+
+    # generating the list of keys based on the rounds number
+    key_list = [[str(random.getrandbits(1)) for nuphin in range(half)] for nuphin in range(rounds)]
+    for i in range(rounds):
+        key_list[i] = "".join(key_list[i])
+
+    
+    if debug == True:
+        print(f"left_before: {left} \nright_before:{right}\n")
+
+    # processing the rounds (that zig-zag thingy)
     for num in range(rounds):
         right_tmp = left
-        left = XOR(lefts=func(side=left, key=k), rights=right)
-        #left = func(side=left, key=k)
+        left = XOR(lefts=XOR(lefts=left, rights=key_list[num]), rights=right)
         right = right_tmp
 
+    # combining halves
     final_message = left + right
     final_message_str = ''
 
+    # converting binary back into str of unicode chars
     for i in range(0, len(final_message), 8):
         final_message_str += chr(int(final_message[i:i+8], 2))
-    return f"left: {left} \nright:{right}\nbinary_message:{binary_message}\nresulting message:{final_message_str}"
-    #return len(binary_message)
+
+    if debug == True:
+        print(f"left_after:  {left} \nright_after: {right}\nbinary_message_input:{binary_message}\n")
+        
+    return final_message_str
 
 
 
 if __name__ == "__main__":
-    print(encrypt("Hello"))
+    print(encrypt("Hello", debug=True))
